40534d8ab14a04eb26d1527e0a78898a7d1b5669
Leonel <mml_leo@hotmail.com>
2017-07-10 03:38:53 -0300

    adding new portifolio page

--- server.js
+++ server.js
@@ -61,6 +61,13 @@ app.get('/bad', (req, res) => {
     })
 })
 
+app.get('/projects',(req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Portifolio page',
+        welcomeMessage: "These are my projects"
+    })
+})
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
